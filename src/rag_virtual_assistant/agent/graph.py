2d38279d59c565cb8adf5992cec1419a114d44d4
David Ruiz <david.ruizcasares@gmail.com>
2026-06-28 00:14:54 +0200

    feat:Added threadID and Checkpointer

--- src/rag_virtual_assistant/agent/graph.py
+++ src/rag_virtual_assistant/agent/graph.py
@@ -1,16 +1,21 @@
 from openai import OpenAI
 from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import MemorySaver
 from rag_virtual_assistant.agent.state import State
 from rag_virtual_assistant.config import Settings
 
 settings = Settings()
 client = OpenAI(api_key=settings.openai_api_key.get_secret_value())
 
+ROLE_MAP = {
+    "human": "user",
+    "ai": "assistant",
+}
+
 def call_model(state: State) -> dict:
     response = client.chat.completions.create(
         model="gpt-5.4-mini",
-        messages = [{"role": m.type if m.type != "human"
-        else "user", "content": m.content} for m in state["messages"]]
+        messages = [{"role": ROLE_MAP.get(m.type, m.type), "content": m.content} for m in state["messages"]],
     )
     content = response.choices[0].message.content
     return {"messages": [{"role": "assistant", "content": content}]}
@@ -22,4 +27,4 @@ builder.add_edge(START, "call_model")
 builder.add_edge("call_model", END)
 
 # 3. Compila
-graph = builder.compile()
+graph = builder.compile(checkpointer=MemorySaver())
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -3,8 +3,10 @@ load_dotenv()
 
 from rag_virtual_assistant.agent.graph import graph
 
-def test_graph(user_message: str):
-    answer = graph.invoke({"messages": [{"role": "user", "content": user_message}]})
+def test_graph(user_message: str, thread_id: str):
+    config = {"configurable": {"thread_id": thread_id}}
+    answer = graph.invoke({"messages": [{"role": "user", "content": user_message}]}, config)
     return answer["messages"][-1].content
 
-print(test_graph("Hola, ¿cómo estás?"))
+print(test_graph("Mi nombre es Juan", "conversacion-1"))
+print(test_graph("¿Cuál es mi nombre?", "conversacion-1"))
